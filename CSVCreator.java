610d8fa91b0be28132a464bfae20474c0bfdf55d
JavierCC <cif23079@uvg,edu.gt>
2023-08-04 08:49:37 -0600

    Csv-reader

---
+++ CSVCreator.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CSVCreator {
+
+    public void crearArchivoCSV(String[] Palindromos) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("palindromas.csv"))) {
+            for (String palabra : Palindromos) {
+                writer.write(palabra);
+                writer.newLine();
+            }
+            System.out.println("Archivo CSV de palabras palíndromas creado correctamente.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al crear el archivo CSV de palabras palíndromas.");
+        }
+    }
+
+    // Puedes agregar otros métodos o atributos a esta clase según tus necesidades.
+}
